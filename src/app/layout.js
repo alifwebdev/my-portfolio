5e11665a58e59fd83831a0e04b161b97fc4410dd
Azizul Islam <alifalamin42@gmail.com>
2026-01-21 12:04:29 +0600

    analytics add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,37 +1,41 @@
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+import Script from "next/script";
 
-import { Montserrat } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/Components/Navbar'
-import Footer from '@/Components/Footer'
-
-
-const montserrat = Montserrat({ subsets: ['latin'], })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Azizul',
-  description: 'Created by Alif',
-}
+  title: "Azizul",
+  description: "Created by Alif",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={""}>
       <head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FW3HW1BQCW"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-FW3HW1BQCW');
-        </script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FW3HW1BQCW"
+          strategy="afterInteractive"
+        />
+
+        <Script id="ga4" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FW3HW1BQCW');
+          `}
+        </Script>
       </head>
-      <body className={`${montserrat.variable} bg-light w-full min-h-screen dark:bg-dark `}>
-        
+      <body
+        className={`${montserrat.variable} bg-light w-full min-h-screen dark:bg-dark `}
+      >
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
       </body>
     </html>
-  )
+  );
 }
