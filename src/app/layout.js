d749fbac22a499824b4306fb0c3b9d311d4dfe02
Azizul Islam <alifalamin42@gmail.com>
2026-01-21 11:46:04 +0600

    Add Google Analytics tracking script to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={""}>
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FW3HW1BQCW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
         
+          gtag('config', 'G-FW3HW1BQCW');
+        </script>
       </head>
       <body className={`${montserrat.variable} bg-light w-full min-h-screen dark:bg-dark `}>
         
