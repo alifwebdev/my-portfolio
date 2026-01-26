72f916669129b92a32d5ff4a6a9822acad6e2966
Azizul Islam <alifalamin42@gmail.com>
2026-01-26 11:39:29 +0600

    courses

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,65 +1,73 @@
-'use client'
-import {motion, useScroll} from "framer-motion"
+"use client";
+import { motion, useScroll } from "framer-motion";
 import { useRef } from "react";
 import LiIcon from "./LiIcon";
 
-const Details = ({type, time, place, info})=>{
-    const ref = useRef(null)
-    return <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
-        <LiIcon reference={ref}/>
-        <motion.div
-        initial={{y:50}}
-        whileInView={{y:0}}
-        transition={{duration:0.5, type:"spring"}}
-        >
-            <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg ">{type}</h3>
-            <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
-                {time} | {place}
-            </span>
-            <p className="font-medium w-full md:text-sm">
-                {info}
-            </p>
-        </motion.div>
+const Details = ({ type, time, place, info }) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg ">
+          {type}
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+          {time} | {place}
+        </span>
+        <p className="font-medium w-full md:text-sm">{info}</p>
+      </motion.div>
     </li>
-}
-
+  );
+};
 
 const Education = () => {
-    const ref = useRef(null);
-    const {scrollYProgress} = useScroll({
-        target: ref,
-        offset: ["start end", "center start"]
-    })
-    return (
-        <div className="my-64">
-            <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
-                Education
-            </h2>
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+  return (
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Education
+      </h2>
 
-            <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
-                <motion.div 
-                style={{scaleY: scrollYProgress}}
-                className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"/>
-                <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-                    <Details 
-                    type="Bachelor Of Science In Computer Science" 
-                    time="2020-2024" 
-                    place="University Of Global Village, Barisal" 
-                    info="The Bachelor Degree in CSE consists of general education courses, mathematics courses, computer core courses, computer project work, computer elective courses. Each student is required to successfully complete a minimum of 136 credit hours to graduate. The student is required to take courses of 132 credits. The remaining 4 credits will be made up of project submission."  />
-                    <Details 
-                    type="Diploma In Computer Science" 
-                    time="2015-2019" 
-                    place="Barguna Polytechnic Institute, Barisal" 
-                    info="Diploma in computer science is a 4-year long program consisting of 8 semesters (1 Semester = 6 months). The final and the midterm exams of each semester are conducted under the supervision of the Bangladesh Technical Education Board (BTEB)."  />
-                    <Details 
-                    type="Complete Web Development (Course)" 
-                    time="2023 (6-month)" 
-                    place="Programming Hero" 
-                    info="Relevant courses included HTML, CSS, Tailwind, JavaScript, React MongoDB, Express JS, Node JS, Firebase, JWT"  />
-                </ul>
-            </div>
-        </div>
-    );
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
+        />
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+          <Details
+            type="Diploma In Computer Science"
+            time="2014-2018"
+            place="Barguna Polytechnic Institute, Barisal"
+            info="Diploma in computer science is a 4-year long program consisting of 8 semesters (1 Semester = 6 months). The final and the midterm exams of each semester are conducted under the supervision of the Bangladesh Technical Education Board (BTEB)."
+          />
+          <Details
+            type="Bachelor Of Science In Computer Science & Engineering"
+            time="2020-2024"
+            place="University Of Global Village, Barisal"
+            info="The Bachelor Degree in CSE consists of general education courses, mathematics courses, computer core courses, computer project work, computer elective courses. Each student is required to successfully complete a minimum of 136 credit hours to graduate. The student is required to take courses of 132 credits. The remaining 4 credits will be made up of project submission."
+          />
+          <Details
+            type="Web Development (On-Going Course)"
+            time="2025-2026 (6-month)"
+            place="Ostad "
+            info="Relevant courses included HTML, CSS, Tailwind, JavaScript, React, Vue, PHP, Laravel, MySql, Firebase, JWT Authentication, RESTful API Development, Git and GitHub for version control, Deployment and Hosting, Responsive Web Design, UI/UX Principles."
+          />
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
